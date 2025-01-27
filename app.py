6cf0f1a098e4567e2f0adbd50dc8a1869828ea00
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-01-27 14:51:30 +0100

    changing stuff

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])    
 def home():
-    return "FART BALLs"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=25565, debug=True)
